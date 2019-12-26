963b850a801c9b19ad63bf9a79ae65941d9ef305
wansf <854347546@qq.com>
2019-12-26 14:17:14 +0800

    Initial Commit

---
+++ UIConfigItemDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIConfigItemDemo
+//
+//  Created by tezwez on 2019/12/26.
+//  Copyright © 2019 tezwez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
